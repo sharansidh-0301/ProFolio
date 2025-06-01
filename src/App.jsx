7763cb4e63eda3b1cdbe805845052b932ed65955
sharansidh-0301 <sharansidh0301@gmail.com>
2025-06-02 02:29:01 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import { Bg } from './components/Bg.jsx'
+import { NavBar } from './components/NavBar.jsx'
+import { Home } from './components/Home.jsx'
+import { Tech } from './components/Tech.jsx'
+import { ReactSection } from './components/ReactSection.jsx'
 import './App.css'
 function App() {
  
   return (
     <>
-         
+           <Bg />
+    <NavBar/>
+    <Home />
+    <Tech/>
+    
+    <ReactSection/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,11 @@ import App from './App.jsx'
 import { Bg } from './components/Bg.jsx'
 import { NavBar } from './components/NavBar.jsx'
 import { Home } from './components/Home.jsx'
-import { Skills } from './components/Skills.jsx'
 import { Tech } from './components/Tech.jsx'
 import { ReactSection } from './components/ReactSection.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Bg />
-    <NavBar/>
-    <Home />
-    <Tech/>
-    <App />
-    <ReactSection/>
+  <App />
   </StrictMode>,
 )
