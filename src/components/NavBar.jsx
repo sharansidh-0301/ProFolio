5087173b74629fbab1590bedc0b26141e1d1c7a0
sharansidh-0301 <sharansidh0301@gmail.com>
2025-06-03 12:37:19 +0530

    Updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
             <div className="container mx-auto flex justify-between items-center">
             <div className="text-white text-lg font-bold">Profolio</div>
             <ul className="flex space-x-4">
-                <li><a href="#home" className="text-white hover:text-gray-400">Home</a></li>
+                <li><a href="" className="text-white hover:text-gray-400">Home</a></li>
                 <li><a href="#about" className="text-white hover:text-gray-400">About</a></li>
                 <li><a href="#project" className="text-white hover:text-gray-400">Projects</a></li>
                 <li><a href="#footer" className="text-white hover:text-gray-400">Contact</a></li>
