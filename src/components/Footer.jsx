ee6107923a0b72105f61cd1bf3ec81085719abd1
sharansidh-0301 <sharansidh0301@gmail.com>
2025-06-27 22:10:10 +0530

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     <>
     <div id='footer' className="footer  w-full bg-gray-800 shadow-lg">
         <div className="container mx-auto px-4 py-4 text-center">
-            <p className="text-gray-400">© 2025 Sharan Sidh. All rights reserved.</p>
+            <p className="text-gray-400">© 2025 Sharan Sidh. All rights reserved. </p>
             <div className="social-links mt-4">
             <a href="https://www.linkedin.com/in/sharansidh0301/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mx-2">LinkedIn</a>
             <a href="https://github.com/sharansidh-0301" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mx-2">GitHub</a>
